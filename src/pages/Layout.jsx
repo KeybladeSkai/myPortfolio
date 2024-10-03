fe02bf834b3bac026ef90eb621289bb29cb76c47
keybladeSkai <asuleodu@gmail.com>
2024-10-03 04:38:54 +0100

    new changes

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,19 +1,38 @@
+import { useEffect } from "react";
 import Header from "../components/header/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const { pathname } = useLocation(); // Track the route change
+
+  useEffect(() => {
+    const scrollableContainer = document.getElementById(
+      "your-scrollable-container"
+    );
+
+    if (scrollableContainer) {
+      // Ensure the scroll happens after the content has loaded
+      setTimeout(() => {
+        scrollableContainer.scrollTop = 0;
+      }, 0);
+    }
+  }, [pathname]); // Run the effect only when the route (pathname) changes
+
   return (
     <div className="bg-black h-[100svh] flex flex-col overflow-hidden relative px-2 md:px-4  md:pt-[20px] ">
       <Header />
-      <div className="overflow-auto overflow-x-hidden h-[100svh] w-full scrollbar-thin md:scrollbar-thin2 bg-white mt-[66px] md:mt-[50px] ">
+      <div
+        id="your-scrollable-container"
+        className="overflow-auto overflow-x-hidden h-[100svh] w-full scrollbar-thin md:scrollbar-thin2 bg-white mt-[66px] md:mt-[50px] "
+      >
         <Outlet />
-        <footer className=" w-[100%]  py-2 text-black  border-black text-center flex flex-col border-t-[1px]">
+        <footer className="w-[100%] py-2 text-black border-black text-center flex flex-col border-t-[1px]">
           <span className="font-medium">
-            @2024, made by <span className="font-bold"> KeybladeSkai </span>
-            with reactjs.
+            @2024, made by <span className="font-bold">KeybladeSkai</span> with
+            React.js.
           </span>
           <span className="mt-[-10px] font-medium">
-            I hope i left a Good impression ㋡ ....
+            I hope I left a good impression ㋡ ....
           </span>
         </footer>
       </div>
