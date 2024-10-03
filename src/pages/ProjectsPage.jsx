7e1359ee70f9e3d90d3ae67850cf40ab527b0fe8
keybladeSkai <asuleodu@gmail.com>
2024-10-03 04:28:32 +0100

    new changes oct 3

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -9,7 +9,7 @@ const ProjectPage = () => {
 
       <div
         style={{ backgroundPosition: "0px 0px", backgroundSize: "6px" }}
-        className="w-[100%] min-h-[100svh]  flex flex-col border-t-[1px] border-black bg-grayMatter bg-repeat bg-[url('https://assets.website-files.com/5e87e737ee7085b9ba02c101/5e87e737ee7085fccd02c10d_pattern-grey.svg')] mt-[-127px] lg:mt-[52px] pt-5   pb-20 md:px-0 text-[4rem] font-extrabold px-4 "
+        className="w-[100%] min-h-[100svh]  flex flex-col border-t-[1px] border-black bg-grayMatter bg-repeat bg-[url('https://assets.website-files.com/5e87e737ee7085b9ba02c101/5e87e737ee7085fccd02c10d_pattern-grey.svg')] mt-[30px] md:mt-[-127px] lg:mt-[52px] pt-5   pb-20 md:px-0 text-[4rem] font-extrabold px-4 "
       >
         <ProjectList />
       </div>
